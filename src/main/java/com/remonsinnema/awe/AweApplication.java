95214cecaf0c928541821d21923909d09949bffc
Remon Sinnema <rssinnema@yahoo.com>
2017-05-26 16:55:59 +0200

    Start Spring Boot application

---
+++ src/main/java/com/remonsinnema/awe/AweApplication.java
@@ -0,0 +1,15 @@
+package com.remonsinnema.awe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+@SuppressWarnings("checkstyle:HideUtilityClassConstructor")
+public class AweApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(AweApplication.class, args);
+  }
+
+}
