56e390229f3a494dca3771fae3799de62bad97d4
Remon Sinnema <rssinnema@yahoo.com>
2017-05-28 10:45:08 +0200

    Improve look & feel

--- src/main/java/com/remonsinnema/awe/AweApplication.java
+++ src/main/java/com/remonsinnema/awe/AweApplication.java
@@ -8,8 +8,8 @@ import javax.servlet.http.HttpServletResponse;
 import org.apache.tomcat.util.http.fileupload.IOUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
@@ -21,9 +21,14 @@ public class AweApplication {
     SpringApplication.run(AweApplication.class, args);
   }
 
-  @RequestMapping(path = "/", produces = MediaType.TEXT_HTML_VALUE)
+  @RequestMapping(path = "/")
   public void index(HttpServletResponse response) throws IOException {
-    try (InputStream content = getClass().getResourceAsStream("/static/index.html")) {
+    staticResource("index.html", response);
+  }
+
+  @RequestMapping(path = "/static/{name}")
+  public void staticResource(@PathVariable("name") String name, HttpServletResponse response) throws IOException {
+    try (InputStream content = getClass().getResourceAsStream("/static/" + name)) {
       IOUtils.copy(content, response.getOutputStream());
     }
   }
